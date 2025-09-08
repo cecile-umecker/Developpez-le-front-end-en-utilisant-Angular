b52ab8acb8a430395f5cd818686709cd3864c7dd
Cécile UMECKER <74008168+cecile-umecker@users.noreply.github.com>
2025-09-08 13:53:41 +0200

    add models, agrement service, add service on home view

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,31 +1,45 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { BehaviorSubject, of } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+import { Olympic } from '../models/Olympic';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new BehaviorSubject<Olympic[] | null | undefined>(undefined);
 
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
-      catchError((error, caught) => {
-        // TODO: improve error handling
-        console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
+      catchError((error) => {
+        console.error('Erreur lors du chargement des données olympiques :', error);
         this.olympics$.next(null);
-        return caught;
+        return of(null);
       })
     );
   }
 
   getOlympics() {
     return this.olympics$.asObservable();
+  }
+
+  getTotalMedalsByCountry() {
+    return this.olympics$.asObservable().pipe(
+      map((olympics) => {
+        if (!olympics) return null; // ou undefined
+        return olympics.map((country) => ({
+          country: country.country,
+          totalMedals: country.participations.reduce(
+            (total, p) => total + p.medalsCount,
+            0
+          ),
+        }));
+      }),
+    );
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -8,11 +9,17 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$!: Observable<Olympic[] | null | undefined>;
+  public medalsByCountry$!: Observable<{ country: string; totalMedals: number }[] | null | undefined>;
+
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+   // this.olympics$ = this.olympicService.getOlympics();
+    this.olympicService.loadInitialData().subscribe();
+    this.medalsByCountry$ = this.olympicService.getTotalMedalsByCountry();
   }
+
+  
 }
